a7f71eb25c392151258f47e30ebd523f1d025453
Ikram <ikram@aptechnorth.edu.pk>
2026-01-28 14:15:57 +0500

    Add project files.

---
+++ GITHUB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GITHUB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
